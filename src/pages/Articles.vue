d641a3ccb06195c3798a56eb1ad533ccfdc7b8fa
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-07-31 12:57:35 +0200

    feat(articles): simple articles page
    Create link and page to display articles on the Jamstack

---
+++ src/pages/Articles.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <h1>Articles</h1>
+    <ul>
+      <li v-for="article in $page.hygraph.articles" :key="article.id">
+        <p><a :href="article.url" target="_blank">{{ article.title }}</a> de {{ article.source.name }} (mise à jour le {{ article.updatedAt }})</p>
+        <p>Sujets :<span v-for="(subject, key) in article.subjects" :key="key"> - {{ subject }}</span></p>
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  hygraph {
+    articles {
+      id
+      title
+      url
+      source {
+        id
+        name
+      }
+      subjects
+      updatedAt
+    }
+  }
+}
+</page-query>
