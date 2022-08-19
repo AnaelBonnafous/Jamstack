5b0b0c456ab13b5323ff6abf2bb0407ee2fdd907
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-08-19 17:31:45 +0200

    feat(articles): create subject filter
    Add buttons to filter articles by subject on the client side

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -1,14 +1,53 @@
 <template>
   <Layout>
     <h1>Articles</h1>
+    <div>
+      <span>Sujets : </span>
+      <button @click="selectedSubject = ''">Tout</button>
+      <button
+        v-for="subject in subjects"
+        @click="selectedSubject = subject"
+        :key="subject"
+      >
+        {{ subject }}
+      </button>
+      <span> ({{ articles.length }} articles)</span>
+    </div>
     <ul>
-      <li v-for="article in $page.hygraph.articles" :key="article.id">
+      <li v-for="article in articles" :key="article.id">
         <p><a :href="article.url" target="_blank">{{ article.title }}</a> de {{ article.source.name }} (mise à jour le {{ article.updatedAt }})</p>
         <p>Sujets :<span v-for="(subject, key) in article.subjects" :key="key"> - {{ subject }}</span></p>
       </li>
     </ul>
   </Layout>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    selectedSubject: "",
+    subjects: [],
+  }),
+  computed: {
+    articles() {
+      if (this.selectedSubject === "") {
+        return this.$page.hygraph.articles;
+      } else {
+        return this.$page.hygraph.articles.filter((article) =>
+          article.subjects.includes(this.selectedSubject)
+        );
+      }
+    },
+  },
+  created() {
+    this.subjects = [
+      ...new Set(
+        this.$page.hygraph.articles.flatMap((article) => article.subjects)
+      ),
+    ];
+  },
+};
+</script>
 
 <page-query>
 query {
